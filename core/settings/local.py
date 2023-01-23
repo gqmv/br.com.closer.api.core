bc5c688999f2e017a5b46587d82067984c0fa93d
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-01-23 13:39:53 -0300

    [#1]  Adequated the usage of environment variables to the best practices.

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,8 +1,5 @@
 from .base import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "u-a$@li2^iel7c31e6-zdb4!yz9utm3i+#00)*=7%55cgm6jic"
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
