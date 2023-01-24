c3ab46ffaabbce8b59ff4aebdc72125eb7b578e5
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-01-24 19:32:55 -0300

    [fix-3] Addition of user registration endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from authentication.models import CustomUser
+from authentication.serializers import RegistrationSerializer
+
+
+class CustomUserViewSet(viewsets.ViewSet):
+    """
+    A viewset to handle user creation and authentication.
+    """
+    queryset = CustomUser.objects.all()
+    
+    def list(self, request):
+        serializer = RegistrationSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    
+    def create(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.create(serializer.validated_data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    
