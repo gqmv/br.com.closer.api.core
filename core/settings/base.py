ac5767b281295ec087c3d7188c820b2dbf283386
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-03-30 09:10:24 -0300

    Fix

--- core/settings/base.py
+++ core/settings/base.py
@@ -146,8 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "/static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = []
 
