71c3c273cda248ed1051003aab0c71ef2376d0f4
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-03-31 16:48:40 -0300

    Fix

--- core/migrations/0001_createsuperuser.py
+++ core/migrations/0001_createsuperuser.py
@@ -15,18 +15,8 @@ def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> Non
     Dynamically create an admin user as part of a migration
     Password is pulled from Secret Manger (previously created as part of tutorial)
     """
-    if os.getenv("GOOGLE_CLOUD_PROJECT", None):
-        client = secretmanager.SecretManagerServiceClient()
-
-        # Get project value for identifying current context
-        _, project = google.auth.default()
-
-        # Retrieve the previously stored admin password
-        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
-        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
-        admin_password = client.access_secret_version(name=name).payload.data.decode(
-            "UTF-8"
-        )
+    if os.environ.get("ADMIN_PASSWORD", None):
+        admin_password = os.environ.get("ADMIN_PASSWORD")
     else:
         admin_password = "test"
 
--- core/settings/base.py
+++ core/settings/base.py
@@ -24,41 +24,6 @@ from google.cloud import secretmanager
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-env = environ.Env(DEBUG=(bool, False))
-env_file = os.path.join(BASE_DIR, ".env")
-
-# Attempt to load the Project ID into the environment, safely failing on error.
-try:
-    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
-except google.auth.exceptions.DefaultCredentialsError:
-    pass
-
-if os.path.isfile(env_file):
-    # Use a local secret file, if provided
-    env.read_env(env_file)
-elif os.environ.get("GITHUB_ACTIONS", None):
-    # Use GitHub Actions secrets
-    env.read_env(io.StringIO(os.environ.get("DJANGO_SETTINGS")))
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
-
-SECRET_KEY = env("SECRET_KEY")
-
-DEBUG = env("DEBUG")
-
-
-# Application definition
-
 INSTALLED_APPS = [
     # Django apps
     "django.contrib.admin",
