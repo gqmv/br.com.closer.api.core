187d7abbbb969ef0ea31efbc42228aea6a49d90c
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-04-05 16:01:33 -0300

    Updated logging

--- core/settings/tests.py
+++ core/settings/tests.py
@@ -1,4 +1,6 @@
 from .base import *
+from google.cloud import logging_v2
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "local-secret-key"
@@ -22,3 +24,39 @@ WHATSAPP_TOKEN = None
 WHATSAPP_NUMBER_ID = None
 
 GOOGLE_FUNCTION_SERVICE_ACCOUNT = "test-service-account"
+
+
+try:
+    logging_client = logging_v2.Client()
+    handler = logging_client.setup_logging()
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "gcp": {
+                "class": "google.cloud.logging_v2.handlers.CloudLoggingHandler",
+                "client": logging_client,
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["gcp"],
+                "level": "INFO",
+            },
+            "root": {
+                "handlers": ["gcp"],
+                "level": "INFO",
+            },
+            "django": {
+                "handlers": ["gcp"],
+                "level": "INFO",
+            },
+            "django.request": {
+                "handlers": ["gcp"],
+                "level": "INFO",
+            },
+        },
+    }
+except:
+    pass
