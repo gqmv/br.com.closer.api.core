57fe1b7675edd7c1493d89b633ee175b9d99de16
MendesHeitor <hemepe18@gmail.com>
2023-04-20 20:42:53 -0300

    first version of the post-register page missing some adjustments in the style missing adjustments in safety
    Co-authored-by: Gabriel Queiroz <gqmv@cin.ufpe.br>

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import user_register
+from .views import user_register, post_register
 
 
 urlpatterns = [
     path("register/", user_register, name="register"),
     path("register/<int:store_id>/", user_register, name="register-affiliated-store"),
+    path("post-register/<str:user_first_name>/", post_register, name="post-register"),
+    path("post-register/<str:user_first_name>/<int:store_id>/", post_register, name="post-register-affiliated-store"),
 ]
--- web/views.py
+++ web/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from stores.models import WelcomeCampaign
 from .forms import UserRegistrationForm
+from authentication.models import CustomUser
+from django.urls import reverse
 
 
 def user_register(request, store_id: int = None):
@@ -13,12 +15,24 @@ def user_register(request, store_id: int = None):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save(store_id=store_id)
-            return HttpResponseRedirect("/")
+            
+
+            #redirect to the right post registration page wether or not the user is affiliated with a store
+            kwargs = {"user_first_name": form.cleaned_data["first_name"]}
+    
+            if store_id:
+                kwargs["store_id"] = store_id
+                reverse_name = "post-register-affiliated-store"
+            else:
+                reverse_name = "post-register"
+            
+            url = reverse(reverse_name, kwargs=kwargs)
+            return HttpResponseRedirect(url)
     else:
         form = UserRegistrationForm()
 
-    #pelo store id pegar a campanha de boas vindas + a loja
 
+    #get the welcome campaign + store by store id
     welcome_campaign = WelcomeCampaign.objects.filter(store__id=store_id)
 
     try:
@@ -27,3 +41,18 @@ def user_register(request, store_id: int = None):
         welcome_campaign = None
     
     return render(request, "registration.html", {"form": form, "welcome_campaign": welcome_campaign})
+
+
+def post_register(request, user_first_name: str, store_id: int = None):
+    """
+    View thats displayed after registration
+    """
+
+    welcome_campaign = WelcomeCampaign.objects.filter(store__id=store_id)
+
+    try:
+        welcome_campaign = welcome_campaign.get()
+    except WelcomeCampaign.DoesNotExist:
+        welcome_campaign = None
+    
+    return render(request, "post_registration.html", {"user_first_name": user_first_name, "welcome_campaign": welcome_campaign})
