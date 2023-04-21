f5371301bda4d5af2dc6b2192bd06850a58aa2e2
MendesHeitor <hemepe18@gmail.com>
2023-04-21 19:23:46 -0300

    corrected redirecting to landing coming from post-registration

--- web/urls.py
+++ web/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("register/", user_register, name="register"),
     path("register/<int:store_id>/", user_register, name="register-affiliated-store"),
     path("post-register/<str:user_first_name>/", post_register, name="post-register"),
-    path("post-register/<str:user_first_name>/<int:store_id>/", post_register, name="post-register-affiliated-store"),
+    path("post-register/<str:user_first_name>/<int:store_id>/", post_register, name="post-register-affiliated-store")
 ]
