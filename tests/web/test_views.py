828129ff0c47172925fbfb7573ba06e3034318a4
MendesHeitor <hemepe18@gmail.com>
2023-05-04 09:38:14 -0300

    first tests on post register implemented... - redirect on sucessfull register form submission - get on the post-register page

--- tests/web/test_views.py
+++ tests/web/test_views.py
@@ -11,6 +11,10 @@ from web.forms import UserRegistrationForm
 
 @pytest.mark.django_db
 class TestUserRegisterView:
+    """
+    TODO: Test the sucessfull post with affiliated store
+    TODO: Test the sucessfull get with affiliated store
+    """
     client = Client()
     url = reverse("register")
 
@@ -39,3 +43,50 @@ class TestUserRegisterView:
         response = self.client.post(self.url, data=form_data)
 
         mock_form_save.assert_not_called()
+
+
+
+@pytest.mark.django_db
+class TestUserPostRegisterView:
+    client = Client()
+    urls = ["register", "register-affiliated-store", "post-register", "post-register-affiliated-store"]
+    kwargs = {"user_first_name": "Jonh"}
+    store_id = 2
+
+    def test_success_redirect(self):
+        form_data = UserRegistrationFormDataFactory.build()
+        self.kwargs["user_first_name"] = form_data["first_name"]
+
+        url = reverse(self.urls[0])
+        response = self.client.post(url, data=form_data)
+
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.has_header("Location")
+        assert response["Location"] == reverse(self.urls[2], kwargs=self.kwargs)
+
+    def test_success_redirect_affiliated_store(self):
+        form_data = UserRegistrationFormDataFactory.build()
+        self.kwargs["user_first_name"] = form_data["first_name"]
+        self.kwargs["store_id"] = self.store_id
+
+        url = reverse(self.urls[1], kwargs={"store_id": self.store_id})
+        response = self.client.post(url, data=form_data)
+
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.has_header("Location")
+        assert response["Location"] == reverse(self.urls[3], kwargs=self.kwargs)
+
+    def test_get(self):
+        url = reverse(self.urls[2], kwargs=self.kwargs)
+        response = self.client.get(url)
+        
+        assert response.status_code == HTTPStatus.OK
+        assert response.templates[0].name == "post_registration.html"
+
+    def test_get_affiliated_store(self):
+        self.kwargs["store_id"] = self.store_id
+        url = reverse(self.urls[3], kwargs=self.kwargs)
+
+        response = self.client.get(url)
+        assert response.status_code == HTTPStatus.OK
+        assert response.templates[0].name == "post_registration.html"
